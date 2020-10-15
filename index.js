db64d675cbad557e86eedb5d4c40a55f520deded
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2020-10-15 20:20:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function (app) {
+  var plugin = {};
+
+  plugin.id = 'my-signalk-plugin';
+  plugin.name = 'My Great Plugin';
+  plugin.description = 'Plugin that does stuff';
+
+  plugin.start = function (options, restartPlugin) {
+    // Here we put our plugin logic
+    app.debug('Plugin started');
+  };
+
+  plugin.stop = function () {
+    // Here we put logic we need when the plugin stops
+    app.debug('Plugin stopped');
+  };
+
+  plugin.schema = {
+    // The plugin schema
+  };
+
+  return plugin;
+};
