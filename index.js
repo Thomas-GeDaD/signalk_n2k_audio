051a459facdc543935cc67388f257be62afbee41
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2020-10-29 10:46:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,21 +24,23 @@ module.exports = function(app) {
   }
 
   plugin.start = function(options) {
+      // Connect
     
     this.simpleCan = new SimpleCan({
       app,
       canDevice: options.candevice,
       preferredAddress: 35,
-      transmitPGNs: [ 130580, 130573, 126998 ],
+      transmitPGNs: [ 130580, 130573 ],
       addressClaim: {  //60928
-        "Unique Number": 139725,
-       // "Manufacturer Code": 'Fusion Electronics',
-        //"Device Function": 130,
+        "Unique Number": 76223,
+        "Manufacturer Code": 'Fusion Electronics', //generates a second device ?
+        "Device Function": 130,
         "Device Class": 'Entertainment',
         "Device Instance Lower": 0,
         "Device Instance Upper": 0,
         "System Instance": 0,
-        "Industry Group": 'Marine'
+        "Industry Group": 'Marine',
+        "Reserved1" : 0
       },
       productInfo: { //126996
         "NMEA 2000 Version": 1301,
@@ -46,7 +48,7 @@ module.exports = function(app) {
         "Model ID": "UD-650",
         "Software Version Code": "2.0.265",
         "Model Version": "FUSION-LINK-1.0",
-        "Model Serial Code": "76223",
+        "Model Serial Code": 76223,
         "Certification Level": 1,
         "Load Equivalency": 1
       }
@@ -54,65 +56,131 @@ module.exports = function(app) {
     this.simpleCan.start()
     app.setPluginStatus(`Connected to ${options.candevice}`)
 
-//130847
     app.on('N2KAnalyzerOut', (n2k) => {
-      if ( n2k.pgn === 59904
+      if ( n2k.pgn == 126720
            && n2k.dst === this.simpleCan.candevice.address
-           && n2k.fields.PGN === 130580
+           && n2k.fields['Manufacturer Code'] === 'Fusion'
+           && n2k.fields['Proprietary ID'] === 'Request Status'
          ) {
-        this.simpleCan.sendPGN({
-          pgn:130580,
-          dst: n2k.src,
-          'Power': 'Yes',
-          'Default Setting': 0,
-          'Tuner Regions': 1,
-          'Max favorites':0
+        console.log("sende config")
+
+        this.simpleCan.sendPGN({ //1
+          pgn:130820,
+          dst: 255,
+          'Manufacturer Code': 419,
+          'Industry Code': 4,
+          'Message ID': 'alive',
+          'A': 128,
+          'A1': 1 ,
+          'A2': 0 ,
+          'A3': 23 ,
+          'A4': 0 ,
+          'A5': 2 ,
+          'A6': 0 ,
+          'A7': 9 ,
+          'A8': 1 ,
         })
+
+        this.simpleCan.sendPGN({ //3
+          pgn:130820,
+          dst: 255,
+          'Manufacturer Code': 419,
+          'Industry Code': 4,
+          'Message ID': 'deviceID',
+          'A': 128,
+          'A1': 13,
+        })
+
+        this.simpleCan.sendPGN({ //33
+            pgn:130820,
+            dst: 255,
+              'Manufacturer Code': 419,
+              'Industry Code': 4,
+              'Message ID': 'Unit Name',
+              'A':128,
+              'Name': 'FUSION',
+          })
+
+        this.simpleCan.sendPGN({ //2
+          pgn:130820,
+          dst: 255,
+            'Message ID': 'Source',
+            'Manufacturer Code': 419,
+            'Industry Code': 4,
+            'A': 128,
+            'Source ID': 11,
+            'Current Source ID': 11,
+            'D': 10,
+            'E': 21,
+            'Source': 'BT',
+        })
+
+        this.simpleCan.sendPGN({ //4
+            pgn:130820,
+            dst: 255,
+              'Message ID': 'Track Info',
+              'Manufacturer Code': 419,
+              'Industry Code': 4,
+              'A': 128,
+              'Transport': 0,
+              'X': 0,
+              'B': 0,
+              'Track #': 0,
+              'Track Count': 0,
+              'E':0,
+              'Track Length':0.000,
+              'G':0.000,
+              'H':0,            
+            })
+
+            this.simpleCan.sendPGN({ //5
+              pgn:130820,
+              dst: 255,
+                'Message ID': 'Track Title',
+                'Manufacturer Code': 419,
+                'Industry Code': 4,
+                'A': 128,
+                'B': 11,
+                'Track': 'Track1',
+              })
+
+              this.simpleCan.sendPGN({ //32 Power state
+                pgn:130820,
+                dst: 255,
+                  'Message ID': 'Track Title',
+                  'Manufacturer Code': 419,
+                  'Industry Code': 4,
+                  'A': 128,
+                  'State': 1,
+                })
+
       }
       
       if (n2k.pgn == 59904
         && n2k.dst === this.simpleCan.candevice.address
         && n2k.fields.PGN === 126998
       ){
-        this.simpleCan.sendPGN({
+        this.simpleCan.sendPGN({ //wrong data in pgn.h
           pgn:126998,
-          dst: n2k.src,
+          dst: 255,
           'Installation Description #1': 'UD-650',
           'Installation Description #2': 'FUSION',
           'Installation Description #3': 'Fusion Electronics Ltd',
         })
-      }
-      
-      
-      if (n2k.pgn == 126208
-          && n2k.dst === this.simpleCan.candevice.address
-          && n2k.fields.PGN === 130573
-        ){
-          this.simpleCan.sendPGN({
-            pgn:130573,
-            dst: n2k.src,
-            'Play support': 'Stop',
-            'Source': 'Bluetooth',
-            'Browse support': 'Track name',
-            'Thumbs support': 'Yes',
-            'Connected':'Yes',
-            'Repeat support': 'Song',
-            'Shuffle support': 'All',
-          })
-        }
+      } 
 
       if (n2k.pgn==126720 || n2k.pgn==130820 
         || n2k.pgn==130816 || n2k.pgn==130820 
         || n2k.pgn==60928 || n2k.pgn==59904 
         || n2k.pgn==130530 || n2k.fields.PGN == 130573 
         || n2k.fields.PGN== 130574){  //!!126720 Comand from Plotter?
-        console.log("watchdata:")
-        console.log(n2k);
+        //console.log("watchdata:")
+        //console.log(n2k);
       }
-    
-    })
-  }
 
+    })   
+    }
+    
   plugin.stop = function() {
   }
 
